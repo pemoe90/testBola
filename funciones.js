ef742725dc2b00456f7ec8d9b446c2d2f708452a
Eduardo Perez <pemoe90@gmail.com>
2017-09-18 10:36:30 +0200

    Cambios tiempo y lugar objetivo
    Se ha ajustado la velocidad de las bolas y el lugar donde aparecen los objetivos

--- funciones.js
+++ funciones.js
@@ -10,21 +10,22 @@ function empezar(){
 
 //funcion para mover a un lugar aleatorio los objetivos
 function moverObjetivos() {
-    var aux; //variable para guardar un numero aleatorio
-    aux = Math.floor((Math.random()* 20) + 10);
+    //Math.floor(Math.random()*(max-min+1)+min);
+    var aux = 0; //variable para guardar un numero aleatorio
+    aux = Math.floor((Math.random()* 16) + 15);//entre 15 y 30
     document.getElementById("Objetivo1").style.left = aux + "%";
-    aux = Math.floor((Math.random()* 20) + 10);
+    aux = Math.floor((Math.random()* 16) + 15);
     document.getElementById("Objetivo2").style.left = aux + "%";
 }
 
 //funcion para empezar la animacion de las bolas
 function animacionBola(){
   var aux; //Variable para guardar el tiempo que tarda la animacion de forma aleatoria
-  aux = Math.floor((Math.random()* 5) + 2);
+  aux = Math.floor((Math.random()* 6) + 3);
   document.getElementById("Circulo1").style.webkitAnimationDuration = aux + "s";
   document.getElementById("Circulo1").style.webkitAnimationPlayState = "running";
 
-  aux = Math.floor((Math.random()* 5) + 2);
+  aux = Math.floor((Math.random()* 6) + 3);
   document.getElementById("Circulo2").style.webkitAnimationDuration = aux + "s";
   document.getElementById("Circulo2").style.webkitAnimationPlayState = "running";
 }
