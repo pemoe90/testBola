5bf4ffc253c8dcb948bd86d2dde8dd2d59d4913d
Eduardo Perez <pemoe90@gmail.com>
2017-10-10 17:17:37 +0200

    Reinicar y botones
    Se ha creado el boton reiniciar para reiniciar la escena, se pueden usar botones para parar las bolas

--- funciones.js
+++ funciones.js
@@ -18,18 +18,43 @@ function moverObjetivos() {
     document.getElementById("Objetivo2").style.left = aux + "%";
 }
 
+
+//funcion para parar las bolas con teclado:
+
+function pararBolasTeclado(event){
+  var tecla = event.keyCode;
+  if (tecla == 87 || tecla == 38) {
+    detenerBola1();
+  }
+
+  else if (tecla == 83 || tecla == 40) {
+    detenerBola2();
+  }
+}
+
 //funcion para empezar la animacion de las bolas
 function animacionBola(){
-  var aux; //Variable para guardar el tiempo que tarda la animacion de forma aleatoria
-  aux = Math.floor((Math.random()* 6) + 3);
+  var aux, aux1; //Variable para guardar el tiempo que tarda la animacion de forma aleatoria
+  aux = Math.floor((Math.random()* 10) + 3);
   document.getElementById("Circulo1").style.webkitAnimationDuration = aux + "s";
-  document.getElementById("Circulo1").style.webkitAnimationPlayState = "running";
 
   aux = Math.floor((Math.random()* 6) + 3);
   document.getElementById("Circulo2").style.webkitAnimationDuration = aux + "s";
+
   document.getElementById("Circulo2").style.webkitAnimationPlayState = "running";
+  document.getElementById("Circulo1").style.webkitAnimationPlayState = "running";
+
+  document.getElementById("BotonEmpezar").style.zIndex = -1;
+  document.getElementById("BotonReiniciar").style.zIndex = 1;
+
+  var boton = document.getElementById("BotonEmpezar");
+  boton.parentNode.removeChild(boton);
 }
 
+
+function reiniciar(){
+  window.location.reload();
+}
 function detenerBola1(){
   document.getElementById("Circulo1").style.webkitAnimationPlayState = "paused";
   document.getElementById("Circulo1").style.zIndex = 1;
